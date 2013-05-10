56ce5fe057e450677f783d2494f076741a7a5c0a
unknown <Darin@PROD-02.vie-del.pri>
2013-05-10 05:56:44 -0700

    First commit

---
+++ assets/www/modules/dashboard/dashboard.js
@@ -0,0 +1,66 @@
+/**
+ * Implements hook_menu().
+ */
+function dashboard_menu() {
+  try {
+    if (drupalgap.settings.debug) {
+      console.log('dashboard_menu()');
+      console.log(JSON.stringify(arguments));
+    }
+    var items = {
+      'dashboard':{
+        'title':'Dashboard',
+        'page_callback':'dashboard_page',
+      },
+    };
+    return items;
+  }
+  catch (error) {
+    alert('dashboard_menu - ' + error);
+  }
+}
+
+/**
+ * Page callback for the dashboard page.
+ */
+function dashboard_page() {
+  try {
+    if (drupalgap.settings.debug) {
+      console.log('dashboard_page()');
+      console.log(JSON.stringify(arguments));
+    }
+    // TODO - Show site logo and the most recent published node that is promoted
+    // to front page.
+    return '';
+    /*var content = {
+      'users':{
+        'theme':'button_link',
+        'path':'user-listing',
+        'text':'View Users',
+      },
+    };*/
+    return content;
+  }
+  catch (error) {
+    alert('dashboard_page - ' + error);
+  }
+}
+
+/**
+ * Implements hook_theme().
+ */
+function dashboard_theme() {
+  try {
+    if (drupalgap.settings.debug) {
+      console.log('dashboard_theme()');
+    }
+    return {
+      'dashboard':{
+        'template':'smashboard',
+      },
+    };
+  }
+  catch (error) {
+    alert('dashboard_theme - ' + error);
+  }
+}
---
+++ gen/com/ThriveFitness/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.ThriveFitness;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
